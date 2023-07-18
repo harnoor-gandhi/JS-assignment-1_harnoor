9743b41f43115947985e1f2477046724540ed32a
harnoor-gandhi <56862515+harnoor-gandhi@users.noreply.github.com>
2023-07-18 12:51:12 +0530

    Update jsassignment2.js

--- jsassignment2.js
+++ jsassignment2.js
@@ -1,26 +1,35 @@
+// Set Interval function
+
+function greet1() {
+  console.log('Print after 3 seconds');
+}
+
+setInterval(greet1, 3000);
+
+
+
 function showTime() {
   let dateTime= new Date();
   let time = dateTime.toLocaleTimeString();
   console.log(time)
-  // Set setTimeout function
-  let display = setInterval(showTime, 3000);
-  
-  function timeout() {
-    console.log('Timeout');
-  }
-  
-  let id = setTimeout(timeout, 5000);
-  console.log('First line');
-}
-// Set Interval function
-function greet() {
-  console.log('Print after every 3 seconds\n Hi');
-}
-setInterval(greet, 3000);
-
-
-function greet(first, last) {
-  console.log('Hello' + ' ' + first + ' ' + last);
 }
 
-setTimeout(greet, 1000, "Hi", 'This is timeout ');
+let display = setInterval(showTime, 3000);
+
+
+// Set setTimeout function
+function timeout() {
+  console.log('Timeout');
+}
+
+let id = setTimeout(timeout, 3000);
+console.log('first message');
+
+
+
+function greet(name, name1) {
+  console.log('Hello' + ' ' + name + ' ' + name1);
+}
+
+setTimeout(greet, 2000, 'name1 ', 'name2');
+
