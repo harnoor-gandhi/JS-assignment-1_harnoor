d8cc04f019f16f21881f7876c8fdc88a7de5eaae
harnoor-gandhi <56862515+harnoor-gandhi@users.noreply.github.com>
2023-07-19 00:29:28 +0530

    Update js-assign3.js

--- js-assign3.js
+++ js-assign3.js
@@ -1,24 +1,3 @@
-// //EXPLICIT
-// var obj = { name: "John", age: 30, city: "New York" };
-// console.log(obj);
-// console.log(typeof(obj));
-// var string=JSON.stringify(obj);
-// console.log(string);
-// console.log(typeof(string));
-
-// //implicit
-// var x=5;
-// var str="10";
-// var z=x+str;
-// console.log(typeof(x));
-// console.log(typeof(str));
-// console.log(typeof(z));
-// console.log(z);
-// console.log((x)+Number(str));
-// let a ="10"-"8";
-// console.log(typeof(a));  
-
-
 // Object to String Conversion:
 let obj = { key: "value" };
 let str = JSON.stringify(obj);
